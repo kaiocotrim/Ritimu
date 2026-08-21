76826ef78fdb4b578c9880a1e34733e02693c27b
Kaio Cotrim <kaio.cotrim@infor.doc>
2026-08-21 11:36:52 -0300

    feat: prisma conectado com o supabase

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,28 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 
-export function Login() {
-    return (
-        <main>
-            <Card>
-                <h1>Tela de login</h1>
-                <Link href="/">
-                    <Button>Voltar para home</Button>
-                </Link>
-            </Card>
-        </main>
-    )
+export default function Login() {
+  return (
+    <main>
+      <Card className="p-6">
+        <h1>Tela de login</h1>
+
+        <Input
+          type="text"
+          placeholder="Digite seu usuário"
+        />
+
+        <Input
+          type="password"
+          placeholder="Digite sua senha"
+        />
+
+        <Link href="/">
+          <Button>Voltar para home</Button>
+        </Link>
+      </Card>
+    </main>
+  )
 }
-
-export default Login 
