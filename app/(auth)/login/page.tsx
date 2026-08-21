565213e19a3c94dce27259c50f8e13b94262d315
kaiocotrim <kaioalan40@gmail.com>
2026-08-20 23:09:23 -0300

    feat: criando o componte de card

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link"
-import { Button }  from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
 
 export function Login() {
     return (
         <main>
-            <h1>Tela de login</h1>
-            <Link href="/">
-                <Button>Voltar para home</Button>
-            </Link>
+            <Card>
+                <h1>Tela de login</h1>
+                <Link href="/">
+                    <Button>Voltar para home</Button>
+                </Link>
+            </Card>
         </main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export function Home() {
   return (
     <main>
-      <h1>Bem-vindo ao RITIMU !</h1>
-      <Link href="/login">
-        <Button>Entrar</Button>
-      </Link>
+      <Card>
+        <h1>Bem-vindo ao RITIMU !</h1>
+        <Link href="/login">
+          <Button>Entrar</Button>
+        </Link>
+      </Card>
+
     </main>
   );
-} 
+}
 
 export default Home;
