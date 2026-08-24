d7c2c392f8e1aff1e7935b42e77fef9a48912a24
Kaio Cotrim <kaio.cotrim@infor.doc>
2026-08-24 09:14:20 -0300

    feat: conecta prisma ao supabase

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma"
 
 export async function GET() {
-  const users = await prisma.user.findMany()
+  const users = await prisma.users.findMany()
 
   return Response.json(users)
 }
